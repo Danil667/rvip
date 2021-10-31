40b15edfcf825886c0074d2a1a1ab98ebeb7c58f
Danila <danil732000@gmail.com>
2021-10-31 22:56:53 +0400

    Добавьте файлы проекта.

---
+++ rvip/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rvip
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LogicalClock logical = new LogicalClock();
+			logical.start();
+			Console.ReadKey();
+		}
+	}
+}
