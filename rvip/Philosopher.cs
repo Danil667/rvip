df29fdeb00a9a7e94d2606ae7a6aae8fc2f8a5d3
Danila <danil732000@gmail.com>
2021-11-14 21:25:27 +0400

    rvip2

---
+++ rvip/Philosopher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace rvip
+{
+    public class Philosopher
+    {
+        bool _isHunger;
+        readonly string _philosopherName;
+        readonly int _number;
+        int _time;
+
+        public Philosopher(string name, int number)
+        {
+            _philosopherName = name;
+            _number = number;
+        }
+
+        void GetFork(IList<Fork> fork)
+        {
+            _time = new Random().Next(System.DateTime.Now.Millisecond);
+
+            Console.WriteLine("Философ " + _philosopherName + " ждет вилку" + "\t ({0}мс)", _time);
+
+            var first = _number;
+            var second = (_number + 1) % (fork.Count - 1);
+
+            if (fork[first].IsUsing || fork[second].IsUsing) return;
+
+            fork[first].IsUsing = true;
+            fork[second].IsUsing = true;
+
+            Console.WriteLine("Философ " + _philosopherName + " обедает" + "\t ({0}мс)", _time);
+            Console.WriteLine("Вилки " + (first + 1) + " и " + (second + 1) + " заняты " + "\t ({0}мс)", _time);
+            Thread.Sleep(_time);
+
+
+
+            fork[first].IsUsing = false;
+            fork[second].IsUsing = false;
+        }
+
+        public void Start(object obj)
+        {
+            while (true)
+            {
+                Thread.Sleep(_time);
+                ChangeStatus();
+                if (_isHunger)
+                    GetFork((List<Fork>)obj);
+            }
+        }
+
+        void ChangeStatus()
+        {
+            _isHunger = !_isHunger;
+            if (!_isHunger)
+                Console.WriteLine("Философ " + _philosopherName + " думает." + "\t ({0}мс)", _time);
+        }
+    }
+
+    public class Fork
+    {
+        public bool IsUsing { get; set; }
+    }
+}
--- rvip/Program.cs
+++ rvip/Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace rvip
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			LogicalClock logical = new LogicalClock();
-			logical.start();
-			Console.ReadKey();
-		}
-	}
+        public static Semaphore Semaphore = new Semaphore(3, 3);
+        static readonly List<Fork> Fork = new List<Fork>();
+        static readonly List<Philosopher> Ph = new List<Philosopher>();
+
+        static void Main()
+        {
+            Console.WriteLine("Введите кол-во философов: ");
+            var count = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < count; i++)
+            {
+                Fork.Add(new Fork());
+                Ph.Add(new Philosopher((i + 1).ToString(), i));
+                new Thread(Ph[i].Start).Start(Fork);
+            }
+        }
+    }
 }
